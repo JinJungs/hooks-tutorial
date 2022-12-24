3052ee51ab2a8a4ead71f2af5d50af915da93b58
JinJungs <nehciyz@gmail.com>
2022-12-24 14:42:47 +0900

    useState, useRef, useCallback, useMemo, useEffect 사용법 연습

---
+++ src/Average.js
@@ -0,0 +1,45 @@
+import { useCallback, useMemo, useRef, useState } from "react"
+
+const getAverage = numbers => {
+    console.log('평균값 계산중')
+    if(numbers.length === 0 ) return 0
+    const sum = numbers.reduce((a,b) => a+b)
+    return sum / numbers.length
+}
+
+const Average = () => {
+    const [list, setList] = useState([])
+    const [number, setNumber] = useState('')
+    const inputE1 = useRef(null)
+
+    const onChange = useCallback(e => {
+        setNumber(e.target.value)
+    },[]) // 컴포넌트가 처음 렌더링될 때만 함수생성
+
+    const onInsert = useCallback(e => { 
+        const nextList = list.concat(parseInt(number))
+        setList(nextList)
+        setNumber('')
+        inputE1.current.focus()
+    }, [number, list]) // number 혹은 리스트가 바뀌었을 때만 함수생성
+
+    const avg = useMemo(() => getAverage(list), [list])
+
+    return (
+        <div>
+            <input value={number} onChange={onChange} ref={inputE1}/>
+            <button onClick={onInsert}>등록</button>
+            <ul>
+                {list.map((value, index) => (
+                    <li key={index}>{value}</li>
+                ))}
+            </ul>
+            <div>
+                {/* 인풋값이 변경될 때도 해당함수를 호출 */}
+                <b>평균값: </b> {avg}
+            </div>
+        </div>
+    )
+}
+
+export default Average
